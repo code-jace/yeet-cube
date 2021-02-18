a85ad2304fe189fe737577d5fceab5cad5338011
Oisin Hennessy <crowregent@gmail.com>
2021-02-18 18:47:24 +0000

    add basic physics - add crosshairs - add yeet on click

---
+++ Yeet Cube/Assets/Scripts/YeetScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YeetScript : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float yeetFactor = 500f;
+    // Update is called once per frame
+    void Update()
+    {
+        // check if moving, if not can yeet
+        if(rb.velocity.magnitude < 0.1f) {
+            if(Input.GetMouseButtonDown(1)){
+                // yeet
+                rb.AddForce(Camera.main.transform.forward*yeetFactor);
+            }
+        }
+
+    }
+
+}

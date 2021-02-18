016af57723c90afc42d9103b86535febeeee445d
Oisin Hennessy <crowregent@gmail.com>
2021-02-18 21:04:53 +0000

    implement new control method

--- Yeet Cube/Assets/Scripts/YeetScript.cs
+++ Yeet Cube/Assets/Scripts/YeetScript.cs
@@ -6,17 +6,20 @@ public class YeetScript : MonoBehaviour
 {
     public Rigidbody rb;
     public float yeetFactor = 500f;
+    bool canYeet = false;
     // Update is called once per frame
     void Update()
     {
         // check if moving, if not can yeet
-        if(rb.velocity.magnitude < 0.1f) {
-            if(Input.GetMouseButtonDown(1)){
-                // yeet
-                rb.AddForce(Camera.main.transform.forward*yeetFactor);
-            }
-        }
+        canYeet = rb.velocity.magnitude < 0.1f;            
 
+    }
+
+    public void FireYeet() {
+        if(canYeet){
+            rb.AddForce(Camera.main.transform.forward*yeetFactor);
+        }
+        
     }
 
 }
